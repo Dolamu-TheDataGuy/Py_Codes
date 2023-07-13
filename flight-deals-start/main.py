bddd463fae490f5421fcab5d2e69b270072d7de7
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-07-13 12:09:38 +0100

    added control flow to check if price is less than lowest price

--- flight-deals-start/main.py
+++ flight-deals-start/main.py
@@ -1,19 +1,46 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
+from datetime import datetime, timedelta
+import json
+
+ORIGIN_CITY = "LDN"
+
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
-print(sheet_data)
+# print(sheet_data)
 
 
-if sheet_data[0]["iataCode"] == "":
+if sheet_data[0]["iataCode"] != "":
+    city_names = [row["city"] for row in sheet_data]
+    print(city_names)
     flight_search = FlightSearch()
     for row in sheet_data:
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet data:\n {sheet_data}")
+    #print(f"sheet data:\n {sheet_data}")
 
     #Update iata code with TESTING to the destination data
     data_manager.destination_data = sheet_data
     # make a PUT (edit) to sheety API
     data_manager.update_destination_codes()
+    with open("sheet.json", "w") as f:
+        json.dump(sheet_data, f, indent=4)
+
+today = datetime.now() + timedelta(1)
+six_month_from_today = datetime.now() + timedelta(6*30)
+
+for destination in sheet_data:
+    flight = FlightSearch().research_flight(
+        ORIGIN_CITY,
+        destination["iataCode"],
+        today,
+        six_month_from_today
+    )
+    
+if flight.prices < destination["lowestPrice"]:
+    NotificationManager().send_notification(
+        message=f"Low price alert! only ${flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}"
+    )
+
--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -1,3 +1,23 @@
+from twilio.rest import Client
+import cred
+
+TWILIO_SID = cred.TWILIO_SID
+TWILIO_AUTH_TOKEN = cred.TWILIO_AUTH_TOKEN
+TWILIO_VIRTUAL_NUMBER = cred.TWILIO_VIRTUAL_NUMBER
+TWILIO_VERIFIED_NUMBER = cred.TWILIO_VERIFIED_NUMBER
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_notification(self, message):
+        message = self.client.message.create(
+            body = message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+
+        #print if successfully sent
+        print(message.sid)
+    
