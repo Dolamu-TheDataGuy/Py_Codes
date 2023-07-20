d012a544cb8cb77471b8f75da08ae8f1f7156ded
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2023-07-20 10:46:17 +0100

    added method to post user info to API and method to retrieve user information from API

---
+++ Projects/Webscraping/wsclick.py
@@ -0,0 +1,38 @@
+import requests
+import click as cl
+from bs4 import BeautifulSoup
+from dataclasses import dataclass
+
+
+@dataclass
+class Podcast:
+    title: str
+    link: str
+    desc: str
+    date: str
+
+def get_data(feed_url):
+    resp = requests.get(feed_url)
+    soup = BeautifulSoup(resp.text, features="xml")
+    return soup
+
+def parse_xml(soup):
+    item = soup.find("item")
+    episode = Podcast(
+        title=item.find("title").text,
+        link=item.find("enclosure")["url"],
+        desc=item.find("description").text,
+        date=item.find("pubDate").text
+        )
+    return episode
+
+@cl.command()
+@cl.argument("feed_url")
+def scrape(feed_url):
+    pod = get_data(feed_url)
+    ep = parse_xml(pod)
+    print(ep.__dict__)
+
+
+if __name__ == "__main__":
+    scrape()
